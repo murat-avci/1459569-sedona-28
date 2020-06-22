774b987bcff93773a2b22be84e6f516143062a63
murat-avci <murat-avci@users.noreply.github.com>
2020-06-22 17:45:01 +0300

    Б5. Документ проходит проверку на валидность
    Ошибки валидации исправлена.

--- js/script.js
+++ js/script.js
@@ -6,18 +6,9 @@ var checkOut = modal.querySelector("[name=check-out]");
 var sumAdults = modal.querySelector("[name=adults]");
 var sumChilds = modal.querySelector("[name=childs]");
 
-var isStorageSupport = true;
-var storage = "";
-
-try {
-  storage = localStorage.getItem("sumChilds");
-} catch (err) {
-  isStorageSupport = false;
-}
-
 form.addEventListener("submit", function(evt) {
-  evt.preventDefault();
   if(!checkIn.value || !checkOut.value || !sumAdults.value || !sumChilds.value) {
+    evt.preventDefault();
     modal.classList.remove("modal-error");
     modal.offsetWidth = modal.offsetWidth;
     modal.classList.add("modal-error");
@@ -32,10 +23,8 @@ form.addEventListener("submit", function(evt) {
 togBtn.addEventListener("click", function(evt) {
   evt.preventDefault();
   modal.classList.add("modal-show");
-    if (storage) {
-      sumChilds.value = storage;
-      sumAdults.focus();
-    } else {
-      sumChilds.focus();
+    if (isStorageSupport) {
+      sumAdults.value = localStorage.getItem("sumAdults");
+      sumChilds.value = localStorage.getItem("sumChilds");      
     }
   });
